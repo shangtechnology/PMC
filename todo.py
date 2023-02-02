476aaf49c9d9ddd66c3fb641f391edf849a2b96d
jim <github@shang.com.au>
2023-02-02 19:09:49 +1000

    Prompt added

--- todo.py
+++ todo.py
@@ -1,6 +1,22 @@
 # Sample
+user_prompt = 'Type add, show, edit or exit: '
+todos = []
+while True:
+    user_action = input(user_prompt)
+    user_action = user_action.strip()
 
+    if user_action == 'add':
+        todo = input ('Enter a todo: ')
+        todos.append(todo)
+    elif user_action == 'show':
+        for item in todos:
+            print(item)
+    elif user_action == 'edit':
+        number = int(input ("number of item to edit"))
+        number = number -1
+        new_todo = input('Ener new todo: ')
+        todos[number] = new_todo
+    else:
+        break
 
-user_prompt = 'Enter to do: '
-user_input = input(user_prompt)
-print(user_input)
+print ('Bye')
